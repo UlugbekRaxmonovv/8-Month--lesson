66a344efea7d18c09b3b2b0ac4a9d15f6a2d4ece
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-06-15 02:47:56 +0500

    main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,11 @@
-
 import './Main.css'
+import rasm from  '../../assets/img/img.png'
 
 const Main = () => {
     return (
         <div>
             <div className="container">
-
+            <img  className='img' src={rasm} alt="" />
             </div>
         </div>
     );
---
+++ src/components/Product/Product.tsx
@@ -0,0 +1,12 @@
+import {useState,useEffect} from 'react';
+import './Product.css'
+
+const Product = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Product;
